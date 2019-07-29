e8be791a583c8d66a6c5ecd26e7cdb098d1b2421
Sam Reinhard <sam@sreinhard.net>
2019-07-29 16:46:18 -0500

    game now displays _ for each letter in the selected word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,17 +2,19 @@
 var words = ["austin", "houston", "dallas", "amarillo", 
                 "marfa", "brownsville", "lubbock", "midland", 
                 "laredo", "terlingua", "odessa", "tyler", 
-                "athens"];
+                "athens", "abilene", "nacogdoches", "texarkana"];
 
 // Computer chooses a word from the array at random
 var selected = words[Math.floor(Math.random() * words.length)];
 console.log(selected); // what did the computer pick
+
+// Replace the letters of the word with "_" and display the result
 var answers = [];
 for (var i = 0; i < selected.length; i++) {
     answers[i] = "_";
 }
 console.log(answers); //did it make an array with _ for each letter?
-$("#hiddenWord").append("stuff");
+$("#hiddenWord").text(answers.join(" "));
 
 
 var lettersLeft = selected.length;
