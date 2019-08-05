85478b2274283600321db1633814ce7b6df30796
Sam Reinhard <sam@sreinhard.net>
2019-08-04 22:35:52 -0500

    added code to match a correct letter to the correct spot in the hidden word, but it doesn't maintain previous correct guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,47 +8,74 @@ var guesses = [];
 var guessesLeft = 6;
 var wins = 0;
 var losses = 0;
-var computerWord = "";
+var selectedWord = "";
+var hiddenWordArray = [];
 
 // Computer chooses a word from the array at random
 function pickSomething(){
-    var selected = words[Math.floor(Math.random() * words.length)];
-    console.log(selected); // what did the computer pick  
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+    console.log(selectedWord);
 
     // Replace the letters of the word with "_" and display the result
-    var answers = [];
-    for (var i = 0; i < selected.length; i++) {
-        answers[i] = "_";
+    for (var i = 0; i < selectedWord.length; i++) {
+        hiddenWordArray[i] = "_";
     }
-    console.log(answers); //did it make an array with _ for each letter?
-    $("#hiddenWord").text(answers.join(" "));
+    console.log(hiddenWordArray); //did it make an array with _ for each letter?
+    $("#hiddenWord").text(hiddenWordArray.join(" "));
 }
 
-// function updateScoreboard(){
-//     $("#guessesLeft").empty();
-//     $("#guessesLeft").append(guessesLeft);
-//     console.log("scoreboard updated");
-// }
+// function for updating the displayed scoreboard
+function updateScoreboard(){
+    $("#guessesLeft").empty();
+    $("#guessesLeft").append(guessesLeft); 
+}
 
 pickSomething();
 
 document.onkeyup = function(event){
     var userGuess = event.key;
-    console.log(userGuess);
 
     // check to see if the user has already guessed the letter
     if (guesses.includes(userGuess) == false){
-        console.log("you haven't guessed that before");
         guesses.push(userGuess);
         console.log(guesses);
         $("#guessed").empty();
-        $("#guessed").append(guesses);
-    }
+        $("#guessed").append(guesses.join(" "));
+
         // if not, check to see if the guess is correct
+        if (selectedWord.includes(userGuess) == true){
+            console.log("yep, that letter is in there");
+            // iterate through the word and replace the underscores with the letter in the matching spot
+            for (var j = 0; j < selectedWord.length; j++){
+                if (userGuess === selectedWord[j]){
+                    console.log("matching the correct letter to the correct space in " + hiddenWord);
+                    var hiddenWordString = hiddenWordArray.join(" ");
+                    var hiddenWordDisplay = hiddenWordString.replace(hiddenWordString[j], userGuess);
+                    console.log(hiddenWordDisplay);
+                    $("#hiddenWord").empty();
+                    $("#hiddenWord").append(hiddenWordDisplay);
+                }
+            }
+        } else {
+            // if not, -1 from guesses left
+            guessesLeft--;
+            updateScoreboard(); 
+        }
 
-            // if so, replace the corresponding _ with the letter, add it to guesses array
+        if (guessesLeft === 0){
+            losses++;
+            guesses = [];
+            guessesLeft = 6;
+            updateScoreboard();
+            pickSomething();
+        }
 
-            // if not, add it to guesses array and -1 from guesses left
+    }
+    // if guessesLeft > 0, add a loss and restart the game
+    // if answers no longer contains _, add a win and restart the game
+            
     
 };
+
+
 
